e7dff5e07404c9a73b7ef5408250fe697e2941a7
Kritika <kritsme963@gmail.com>
2022-11-16 11:24:35 -0800

    Added:create linked list of 56,30,70

--- src/com/datastructure/LinkedList.java
+++ src/com/datastructure/LinkedList.java
@@ -1,8 +1,33 @@
 package com.datastructure;
 
 public class LinkedList {
+    private ListNode head;
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public void display(){
+        ListNode current=head;
+        while(current!=null){
+            System.out.print(current.data+"-->");
+            current=current.next;
+        }
+        System.out.print("null");
+    }
     public static void main(String[] args) {
-        System.out.println("Linked list program operation");
+        LinkedList sll=new LinkedList();
+        sll.head=new ListNode(56);
+        ListNode second=new ListNode(30);
+        ListNode third=new ListNode(70);
+        sll.head.next=second;
+        second.next=third;
+        sll.display();
+        System.out.print("Linked list program operation");
     }
 
 }
